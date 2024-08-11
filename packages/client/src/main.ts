231522ef6753fe8f4595ddb4e788e8e7d6beb12b
shiyuu <tamemotoyou@gmail.com>
2024-08-11 09:27:32 +0900

    feat: client init

---
+++ packages/client/src/main.ts
@@ -0,0 +1,7 @@
+import './assets/styles/tailwind.css'
+import './assets/styles/config.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
---
+++ packages/client/vite.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    esbuild: {
+      drop: mode === 'production' ? ['console', 'debugger'] : []
+    },
+    server: {
+      port: 8080
+    }
+  }
+})
